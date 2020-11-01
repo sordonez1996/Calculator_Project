bf31e4aecec58414493150cf7848686863b6cf31
sordonez1996 <so235@njit.edu>
2020-11-01 18:24:08 -0500

    Feature: Made Addition method and ran tests

---
+++ src/CalculatorTests.py
@@ -0,0 +1,22 @@
+import unittest
+from CsvReader import CsvReader
+from Calculator import Calculator
+from pprint import pprint
+
+class MyTestCase(unittest.TestCase):
+    def setUp( self ) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_results_addition_calculator( self ):
+        test_data = CsvReader('/src/Addition.csv').data
+        pprint(test_data)
+        for row in test_data:
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
+
+if __name__ == '__main__':
+   unittest.main()
