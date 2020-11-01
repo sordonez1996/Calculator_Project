1958ff16b8cbf676a827f8e3ae022aaa96570fac
sordonez1996 <so235@njit.edu>
2020-11-01 18:26:58 -0500

    Feature: Added subtraction

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,6 +1,10 @@
 def addition(a, b):
     return int(a) + int(b)
 
+def subtraction(a, b):
+    return int(a) - int(b)
+
+
 class Calculator:
     result = 0
 
@@ -9,4 +13,8 @@ class Calculator:
 
     def add( self, a, b ):
         self.result = addition(a, b)
+        return self.result
+
+    def subtract( self, a, b ):
+        self.result = subtraction(a, b)
         return self.result
