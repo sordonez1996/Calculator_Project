918088cb0afb609c1ad954667e0644f51b2d0201
sordonez1996 <so235@njit.edu>
2020-11-01 18:16:50 -0500

    Feature: Created class for CsvReader

---
+++ src/CsvReader.py
@@ -0,0 +1,17 @@
+import csv
+
+def ClassFactory(class_name, dictionary):
+    return type(class_name, (object,), dictionary)
+
+
+class CsvReader:
+    data = []
+
+    def __init__(self, filepath):
+        self.data = []
+
+        with open(filepath) as text_data:
+            csv_data = csv.DictReader(text_data, delimiter=',')
+            for row in csv_data:
+                self.data.append(row)
+        pass
